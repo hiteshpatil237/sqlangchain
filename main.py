2a0f088872e2ac5193e097847e82e2d14fd23c6e
hiteshpatil237 <hiteshpatil237@gmail.com>
2024-06-11 09:36:36 -0400

    Reverting back to original code

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from openai import OpenAI
 from langchain_utils import invoke_chain
